9ed5d3ad18457cb9113c67a7e4285b34f71bb7c4
jhorman villanueva <jhorman.villanueva@unad.edu.co>
2024-12-06 18:32:23 +0000

    http server ok

--- server.py
+++ server.py
@@ -1,18 +1,41 @@
-#Suscribe to mqtt broker and publish messages to a topic
-#This is a simple example of how to use the paho-mqtt library to subscribe to a topic and publish messages to a topic
-
-
-#import paho.mqtt.client as mqtt
-import time
-
-from flask import Flask
+from flask import Flask, request
+import csv
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+
+
+def save_to_csv(temperature, humidity):
+    with open('data.csv', 'a', newline='') as csvfile:
+        fieldnames = ['time','temperature', 'humidity']  
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  
+
+        # Check if the file is empty to write header  
+        csvfile.seek(0, 2)  # Move the cursor to the end of the file  
+        if csvfile.tell() == 0: # If file is empty, write header
+            writer.writeheader()
+        
+        timezone = pytz.timezone("America/Bogota")
+        now = datetime.now(timezone)
+        formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        
+        writer.writerow({'time': formatted_time, 'temperature': temperature, 'humidity': humidity})
+
+@app.route('/data', methods=['POST'])
+def data_received():
+    humidity = request.form['humidity']
+    temperature = request.form['temperature']
+    print(f"Humidity: {humidity}%, temperature: {temperature}°C")
+    save_to_csv(temperature,humidity)
+
+    return 'Data received', 200
+
+@app.route('/test')
+def test():
+    return "Hello from Gitpod"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
